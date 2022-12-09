cd0b5ff0f08b5887fb4e908dd2c56a031e9675f2
Murilo <mbalves1@outlook.com>
2022-12-09 19:18:13 -0300

    [feat] - created workspace config in package.json

--- projects/web-public/pages/index.tsx
+++ projects/web-public/pages/index.tsx
@@ -1,7 +1,10 @@
+import { sum } from "@monorepo/utils/math/sum";
+
 export default function HomeScreen(){ 
   return (
     <main>
       <h1> Home </h1>
+      <p>Importanto modulo local: @monorepo/utils/math/sum(2,5) = {sum(2, 5)}</p>
     </main>
   )
 }
